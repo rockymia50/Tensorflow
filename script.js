c159355bdb7b1a0ecc8f912d621764527857497d
Roger McIntosh <rogermcintosh@Rogers-Air.fios-router.home>
2019-07-01 08:13:26 -0400

    update plot

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-console.log('Hello World');
 
-async function get Data() {
-    const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
-    const carsData = await carsDataReq.json();
+async function getData() {
+    const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');  
+    const carsData = await carsDataReq.json();  
     const cleaned = carsData.map(car => ({
-        mpg: car.Miles_per_Gallon,
-        hoursepower: car.Horsepower,
+      mpg: car.Miles_per_Gallon,
+      horsepower: car.Horsepower,
     }))
-    .filter(car => (car.mpg != null && car.hoursepower != null ));
-
+    .filter(car => (car.mpg != null && car.horsepower != null));
+    
     return cleaned;
-}
+  }
 
 
-
-
-async function run() {
-// Load and plot the original input data that we are going to train
-const data = await getData();
-const values = data.map(d => ({
-    x: d.Horsepower,
-    y: d.mpg
-}))   
-}
+  async function run() {
+    // Load and plot the original input data that we are going to train on.
+    const data = await getData();
+    const values = data.map(d => ({
+      x: d.horsepower,
+      y: d.mpg,
+    }));
+  
+    tfvis.render.scatterplot(
+      {name: 'Horsepower v MPG'},
+      {values}, 
+      {
+        xLabel: 'Horsepower',
+        yLabel: 'MPG',
+        height: 300
+      }
+    );
+  
+    // More code will be added below
+  }
+  
+  document.addEventListener('DOMContentLoaded', run);
